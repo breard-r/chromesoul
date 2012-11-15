17a168956076455f6c6b00c892d8846813544300
Rodolphe Breard <rodolphe.breard@uraniborg.net>
2012-11-15 01:45:45 +0100

    improved tabs

--- start.js
+++ start.js
@@ -1,8 +1,15 @@
-var ui = new Nsui(),
-    cs = new NsClient();
+var om, ui, cs;
 
+
+om = new OptionsManager();
+om.init();
+
+ui = new Nsui();
 ui.init();
+
+cs = new NsClient();
 cs.init();
+
 ui.setReconnect(function() {
     cs.disconnect();
     cs.connect();
