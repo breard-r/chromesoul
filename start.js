ba7ef4f2b031035cbb8cd14da3fc4b3d83c434bd
Rodolphe Breard <rodolphe.breard@uraniborg.net>
2012-11-26 15:31:15 +0100

    ability to receive messages

--- start.js
+++ start.js
@@ -1,17 +1,18 @@
-(function() {
-    var om, ui, cs;
+var chromesoul = (function() {
+    var chromesoul = {
+	"opts": new OptionsManager(),
+	"ui": new Nsui(),
+	"client": new NsClient()
+    };
 
-    om = new OptionsManager();
-    om.init();
-
-    ui = new Nsui();
-    ui.init();
-
-    cs = new NsClient();
-    cs.init();
-
-    ui.setReconnect(function() {
-	cs.disconnect();
-	cs.connect();
-    });
+    return (window.chromesoul = window.$cs = chromesoul);
 })();
+
+$cs.opts.init();
+$cs.ui.init();
+$cs.client.init();
+
+$cs.ui.setReconnect(function() {
+    $cs.client.disconnect();
+    $cs.client.connect();
+});
